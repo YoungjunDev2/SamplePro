333a6e472f82fcc4ffaf1d483be53bdf1e9ba48b
96youngjun <96youngjun@gmail.com>
2022-03-07 15:36:11 +0900

    home working.2

--- src/GitTestMain.java
+++ src/GitTestMain.java
@@ -22,6 +22,7 @@ public class GitTestMain {
 		System.out.println("hotfix remote push");
 		System.out.println("hotfix remote merge.");
 		System.out.println("home working");
+		System.out.println("home working.2");
 	}
 
 }
